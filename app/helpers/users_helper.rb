f95570eeb25aba5e21892ce69cad2621bad016d3
Stefan Dili <stefan.dili.021@gmail.com>
2020-05-01 17:07:41 +0200

    remade form with form_tag and form_for

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
     def param_filter
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def param_filter2
+        params.permit(:username, :email, :password)
+    end
 end
